32539d09dd71dd8ec29a5af2aee36c2dd390575f
Dara <bdaralan@gmail.com>
2019-07-31 13:31:42 -0700

    Create RowCenterButtonStyle

---
+++ ShoB/View Modifier/RowCenterButton.swift
@@ -0,0 +1,42 @@
+//
+//  RowCenterButton.swift
+//  ShoB
+//
+//  Created by Dara Beng on 7/31/19.
+//  Copyright © 2019 Dara Beng. All rights reserved.
+//
+
+import SwiftUI
+
+
+struct RowCenterButtonStyle: ButtonStyle {
+    
+    let actionType: ActionType
+    
+    
+    init(_ type: ActionType = .normal) {
+        actionType = type
+    }
+    
+    
+    func makeBody(configuration: Configuration) -> some View {
+        HStack(alignment: .center) {
+            Spacer()
+            configuration.label
+            Spacer()
+        }
+        .foregroundColor(accentColor(for: actionType))
+    }
+    
+    func accentColor(for type: ActionType) -> Color {
+        switch type {
+        case .normal: return .accentColor
+        case .destrictive: return .red
+        }
+    }
+    
+    enum ActionType {
+        case normal
+        case destrictive
+    }
+}
